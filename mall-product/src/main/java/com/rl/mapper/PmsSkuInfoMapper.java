802c2a4b10f0e95008483f1d228963152be5439b
2224605219 <2224605219@qq.com>
2021-10-16 16:05:01 +0800

    baomidou

---
+++ mall-product/src/main/java/com/rl/mapper/PmsSkuInfoMapper.java
@@ -0,0 +1,20 @@
+package com.rl.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.rl.entity.PmsSkuInfo;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author ronglin
+ * @version 1.0
+ * @crate_time 2021/10/16 15:59
+ */
+@Mapper
+public interface PmsSkuInfoMapper extends BaseMapper<PmsSkuInfo> {
+
+
+
+
+
+}
